a8845d48fe27c884601d22b2d90982eccd5429cf
JohannaNunez <johamarisol@outlook.com>
2019-09-24 22:00:28 -0300

    agregando lokiJS para almacenamiento de datos

--- api.js
+++ api.js
@@ -1,20 +1,44 @@
+//Modulos
 const express = require("express")
 const bodyParser = require ("body-parser")
+const loki = require("lokijs")
+//auxiliares o helper
 const server = express()
-
 const port = 2000
 const header = { "Content-Type" : "application/json; charset=utf-8"}
 
+let personas = null
+
+const db = new loki("datos.json", {
+	autoload : true, //true los carga, y false los pisa. resetea la base cada vez que se arranca la app.
+	autosave : true ,// true para que autosalve
+	autosaveInternal : 5000,//tiempo en milisegundos
+	autoloadCallback :  () => {//ar function//instrucciones luego del autoload,
+		
+		personas = db.getCollection("personas") || db.addCollection("personas")
+		/*personas = db.getCollection("personas")
+		if ( personas === null){
+			personas = db.addCollection("personas") //obtener la coleccion. si me arroja nulo es que no existia, entonces la creo
+		}/*/
+	}
+})
+
+
+//configuraciones
 server.listen( port )
 server.use ( bodyParser.urlencoded({ extended : false }))
 server.use ( bodyParser.json() )
-
+//procesos
 server.get("/api", (req, res) => {  //require y response
 	res.set ( header ) //header de la rta de la peticion http del server
- 	res.end("aca devolveré datos en formato json")
+ 	res.json( personas.data )
 }) 
 
 server.post("/api", (req, res) => {
+
+	let persona = req.body
+	personas.insert(persona) //persona es un registro, que se va a llenar con el hipotetico formulario que haga.
+
 	res.set( header )
-	res.json(req.body)
+	res.json( { "rta" : "ok"})
 })
